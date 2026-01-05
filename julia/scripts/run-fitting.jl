fd22742835d33baeef46be7e54ff80d727423a42
chriswaudby <cwaudby@gmail.com>
2026-01-05 08:38:03 +0000

    move ph module up a level

---
+++ julia/scripts/run-fitting.jl
@@ -0,0 +1,17 @@
+include("../src/PHIndicators.jl")
+using .PHIndicators
+##
+# For 2-pKa indicators like maleate:
+indicator_props = Dict(
+    "HEPES1" => IndicatorProperties("HEPES1", "H", 2, 0, [1.9, 7.5], false)
+)
+
+results = run_analysis("../data/group1-rough-h2o.csv";
+    temperature=298.0,
+    ionic_strength=0.3,
+    pKa_reference=6.7,
+    indicator_props=indicator_props
+)
+
+# Check calibration order
+println(results.bootstrap.calibration_order)
